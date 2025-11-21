71e313691f7e4841406bb550186819a8de1b3a1b
Jasmine Tam <jasminetam94@gmail.com>
2025-11-21 23:35:04 +0000

    Initial commit

---
+++ test/myapp-infra.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MyappInfra from '../lib/myapp-infra-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/myapp-infra-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MyappInfra.MyappInfraStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
